8aa6666e2ef5108b4ca8328274bb87f3a13abef8
Felipe <felipemaines123@gmail.com>
2024-05-21 13:58:25 -0300

    edit-delete-70

--- scr/mod_funcionario/funcionario.py
+++ scr/mod_funcionario/funcionario.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, jsonify
 import requests
 from mod_login.login import validaSessao
+from funcoes import Funcoes
 from settings import getHeadersAPI, ENDPOINT_FUNCIONARIO
 bp_funcionario = Blueprint('funcionario', __name__, url_prefix="/funcionario", template_folder='templates')
 
@@ -21,3 +22,117 @@ def formListaFuncionario():
         return render_template('formListaFuncionario.html', msgErro=e.args[0])
 
 """ restante dos dados omitidos """
+
+@bp_funcionario.route('/form-funcionario/',)
+def formFuncionario():
+    return render_template('formFuncionario.html')
+
+@bp_funcionario.route('/insert', methods=['POST'])
+def insert():
+    try:
+        # dados enviados via FORM
+        id_funcionario = 0
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        grupo = request.form['grupo']
+        senha = Funcoes.get_password_hash(request.form['senha'])
+        
+        # monta o JSON para envio a API
+        payload = {
+            'id_funcionario': id_funcionario,
+            'nome': nome,
+            'matricula': matricula,
+            'cpf': cpf,
+            'telefone': telefone,
+            'grupo': grupo,
+            'senha': senha
+        }
+        
+        # executa o verbo POST da API e armazena seu retorno
+        response = requests.post(ENDPOINT_FUNCIONARIO, headers=getHeadersAPI(), json=payload)
+        result = response.json()
+        print(result)  # [{'msg': 'Cadastrado com sucesso!', 'id': 13}, 200]
+        print(response.status_code)  # 200
+        
+        if response.status_code != 200 or result[1] != 200:
+            raise Exception(result)
+        
+        return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+    
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
+    
+@bp_funcionario.route("/form-edit-funcionario", methods=['POST'])
+def formEditFuncionario():
+    try:
+        # ID enviado via FORM
+        id_funcionario = request.form['id']
+
+        # executa o verbo GET da API buscando somente o funcionário selecionado,
+        # obtendo o JSON do retorno
+        response = requests.get(ENDPOINT_FUNCIONARIO + id_funcionario, headers=getHeadersAPI())
+        result = response.json()
+
+        if response.status_code != 200:
+            raise Exception(result)
+
+        # renderiza o form passando os dados retornados
+        return render_template('formFuncionario.html', result=result[0])
+
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
+
+@bp_funcionario.route('/edit', methods=['POST'])
+def edit():
+    try:
+        # Dados enviados via FORM
+        id_funcionario = request.form['id']
+        nome = request.form['nome']
+        matricula = request.form['matricula']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        grupo = request.form['grupo']
+        senha = Funcoes.cifraSenha(request.form['senha'])
+
+        # Monta o JSON para envio a API
+        payload = {
+            'id_funcionario': id_funcionario,
+            'nome': nome,
+            'matricula': matricula,
+            'cpf': cpf,
+            'telefone': telefone,
+            'grupo': grupo,
+            'senha': senha
+        }
+
+        # Executa o verbo PUT da API e armazena seu retorno
+        response = requests.put(ENDPOINT_FUNCIONARIO + id_funcionario, headers=getHeadersAPI(), json=payload)
+        result = response.json()
+
+        if response.status_code != 200 or result[1] != 200:
+            raise Exception(result)
+
+        return redirect(url_for('funcionario.formListaFuncionario', msg=result[0]))
+
+    except Exception as e:
+        return render_template('formListaFuncionario.html', msgErro=e.args[0])
+
+@bp_funcionario.route('/delete', methods=['POST'])
+def delete():
+    try:
+        # Dados enviados via FORM
+        id_funcionario = request.form['id']
+
+        # Executa o verbo DELETE da API e armazena seu retorno
+        response = requests.delete(ENDPOINT_FUNCIONARIO + id_funcionario, headers=getHeadersAPI())
+        result = response.json()
+
+        if response.status_code != 200 or result[1] != 200:
+            raise Exception(result)
+
+        return jsonify(erro=False, msg=result[0])
+
+    except Exception as e:
+        return jsonify(erro=True, msgErro=e.args[0])
