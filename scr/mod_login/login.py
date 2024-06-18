682a897afaea36ae3d1561aef6d73a902b6873b6
Vit0rSilv4 <vitordmvb@uniplaclages.edu.br>
2024-06-18 17:38:54 -0300

    18-06

--- scr/mod_login/login.py
+++ scr/mod_login/login.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
 import requests
-from settings import ENDPOINT_TOKEN
+from settings import ENDPOINT_TOKEN, SECRET_KEY, ALGORITHM
 from datetime import datetime, timedelta
+from jose import jwt
 bp_login = Blueprint('login', __name__, url_prefix='/', template_folder='templates')
 from functools import wraps
 
@@ -14,42 +15,82 @@ def validaLogin():
     try:
         session.clear()
         headers = {
-        'accept': 'application/json',
-        'Content-Type': 'application/x-www-form-urlencoded'
-        }       
+            'accept': 'application/json',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
         data = {
-        'username': request.form['usuario'],
-        'password': request.form['senha'],
-        'grant_type': '', 'scope': '', 'client_id': '', 'client_secret': ''
+            'username': request.form['usuario'],
+            'password': request.form['senha'],
+            'grant_type': '', 'scope': '', 'client_id': '', 'client_secret': ''
         }
         response = requests.post(ENDPOINT_TOKEN, headers=headers, data=data)
         access_token = response.json()
-        if (response.status_code != 200):
+        if response.status_code != 200:
             raise Exception(access_token)
-        
-        # registra os dados do token e do usuário na sessão, armazenando o login do usuário
+
+        # Decodifica o token para extrair as informações
+        token_payload = jwt.decode(access_token['access_token'], SECRET_KEY, algorithms=[ALGORITHM])
+
+        # Registra os dados do token e do usuário na sessão, armazenando o login do usuário
         session['access_token'] = access_token['access_token']
-        session['expire_minutes'] = access_token['expire_minutes']
+        session['expire_minutes'] = (token_payload['exp'] - datetime.now().timestamp()) / 60  # Calcula o tempo de expiração em minutos
         session['token_type'] = access_token['token_type']
-        session['token_validade'] = datetime.timestamp( datetime.now() + timedelta(minutes=access_token['expire_minutes']) )
-        ### futuramente alterar a api para retornar os dados do usuário
+        session['token_validade'] = token_payload['exp']
+        ### Futuramente alterar a API para retornar os dados do usuário
         session['nome'] = request.form['usuario']
         session['login'] = request.form['usuario']
         session['grupo'] = "1"
-        # abre a aplicação na tela home
+        # Abre a aplicação na tela home
         return redirect(url_for('index.formIndex'))
     except Exception as e:
-    # retorna para a tela de login
+        # Retorna para a tela de login
         return redirect(url_for('login.login', msgErro=e.args[0], msgException=e.args[0]))
-    
+
 @bp_login.route("/logoff", methods=['GET'])
 def logoff():
-    # limpa um valor individual
+    # Limpa um valor individual
     session.pop('login', None)
-    # limpa toda sessão
+    # Limpa toda sessão
     session.clear()
-    # retorna para a tela de login
+    # Retorna para a tela de login
     return redirect(url_for('login.login'))
+
+def is_token_expired():
+    token_validade = session.get('token_validade')
+    if token_validade:
+        return datetime.now().timestamp() > token_validade
+    return True
+
+def refresh_token():
+    try:
+        access_token = session.get('access_token')
+        headers = {
+            'Authorization': f'Bearer {access_token}',
+            'Content-Type': 'application/json'
+        }
+        response = requests.post(f"{ENDPOINT_TOKEN}/refresh-token", headers=headers)
+        if response.status_code == 200:
+            new_token = response.json()
+            session['access_token'] = new_token['access_token']
+            session['token_type'] = new_token['token_type']
+            # Atualiza a validade do token na sessão
+            token_payload = jwt.decode(new_token['access_token'], SECRET_KEY, algorithms=[ALGORITHM])
+            session['token_validade'] = token_payload['exp']
+        else:
+            raise Exception("Erro ao renovar token")
+    except Exception as e:
+        print(f"Erro ao renovar token: {e}")
+        return redirect(url_for('login.login', msgErro="Erro ao renovar token"))
+
+def fetch_protected_data(url):
+    if is_token_expired():
+        refresh_token()
+    access_token = session.get('access_token')
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
+    response = requests.get(url, headers=headers)
+    return response.json()
 
 def validaSessao(f):
     @wraps(f)
@@ -63,9 +104,8 @@ def validaSessao(f):
 def validaToken(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if 'token_validade' in session and session['token_validade'] > datetime.timestamp( datetime.now() ):
+        if 'token_validade' in session and session['token_validade'] > datetime.timestamp(datetime.now()):
             return f(*args, **kwargs)
         else:
             return redirect(url_for('login.login', msgErro='Usuário não logado! Token expirado!'))
-
     return decorated_function
--- scr/settings.py
+++ scr/settings.py
@@ -1,26 +1,34 @@
 from dotenv import load_dotenv, find_dotenv
 import os
 from flask import session
-# localiza o arquivo de .env
+
+# Localiza o arquivo .env
 dotenv_file = find_dotenv()
 # Carrega o arquivo .env
 load_dotenv(dotenv_file)
+
 # Configurações da APP
 HOST = os.getenv("HOST")
 PORT = os.getenv("PORT")
 DEBUG = os.getenv("DEBUG")
+
 # Configurações da API
 URL_API = os.getenv("URL_API")
+
 # Configuração dos endpoints
 ENDPOINT_TOKEN = os.getenv("ENDPOINT_TOKEN")
 ENDPOINT_FUNCIONARIO = os.getenv("ENDPOINT_FUNCIONARIO")
 ENDPOINT_CLIENTE = os.getenv("ENDPOINT_CLIENTE")
 ENDPOINT_PRODUTO = os.getenv("ENDPOINT_PRODUTO")
+
 # Configurações de segurança
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
 def getHeadersAPI():
     return {
-    'accept': 'application/json',
-    'Authorization': f'Bearer {session['access_token'] if 'access_token' in session else ""}'
-}
+        'accept': 'application/json',
+        'Authorization': f'Bearer {session["access_token"] if "access_token" in session else ""}'
+    }
 
 TEMPO_SESSION = os.getenv("TEMPO_SESSION")
